10eb873e8873762fb17aa2877f0214743ff69827
HTarzwell <htarzwell@gmail.com>
2018-09-17 18:45:07 -0400

    Started building new player logic

--- assets/scripts/games/players.js
+++ assets/scripts/games/players.js
@@ -1,12 +1,26 @@
 'use strict'
 
-const tokenOne = 'X'
-const tokenTwo = 'O'
-let roundNumber = 0
+const gamePlayer = {
+  let token = '',
+  let roundNumber = 0,
+  function switchToken () {
+    if (this.roundNumber === 0 || this.roundNumber % 2 !== 1) {
+      this.token = 'X'
+    } else {
+      this.token = 'O'
+    }
+    console.log('token is ', this.token)
+    console.log('round number is ', this.roundNumber)
+  }
+}
 
-function changeToken = function () {
-  
+function changeToken () {
+  $('.col button').on('click', function () {
+    gamePlayer.roundNumber += 1
+  })
+  .then(gamePlayer.switchToken)
 }
 
 module.exports = {
+  gamePlayer
 }
