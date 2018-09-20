e1163241bf46b0c2d15e1b56d3736878c8d002b4
HTarzwell <htarzwell@gmail.com>
2018-09-20 12:06:49 -0400

    Added board assignments for new

--- assets/scripts/games/board.js
+++ assets/scripts/games/board.js
@@ -1,21 +1,50 @@
 'use strict'
 const player = require('./players')
 
-// let board = new Array(9).fill('')
+let board = {
+ square1: '',
+ square2: '',
+ square3: '',
+ square4: '',
+ square5: '',
+ square6: '',
+ square7: '',
+ square8: '',
+ square9: ''
+}
 
-function selectSquare () {
-  
+function switchToken () {
+  if (player.gamePlayer.roundNumber === 0 || player.gamePlayer.roundNumber % 2 !== 1) {
+    player.gamePlayer.token = 'X'
+  } else {
+    player.gamePlayer.token = 'O'
+  }
 }
 
 function playToken () {
-    if (player.gamePlayer.roundNumber === 0 || player.gamePlayer.roundNumber % 2 !== 1) {
-      player.gamePlayer.token = 'X'
-    } else {
-      player.gamePlayer.token = 'O'
-    }
+    switchToken()
     player.gamePlayer.roundNumber += 1
     $(this).text(player.gamePlayer.token)
     $(this).off()
+    if ($(this).prop('id') === 'btn-1') {
+      board.square1 = $(this).text()
+    } else if ($(this).prop('id') === 'btn-2') {
+      board.square2 = $(this).text()
+    } else if ($(this).prop('id') === 'btn-3') {
+      board.square3 = $(this).text()
+    } else if ($(this).prop('id') === 'btn-4') {
+      board.square4 = $(this).text()
+    } else if ($(this).prop('id') === 'btn-5') {
+      board.square5 = $(this).text()
+    } else if ($(this).prop('id') === 'btn-6') {
+      board.square6 = $(this).text()
+    } else if ($(this).prop('id') === 'btn-7') {
+      board.square7 = $(this).text()
+    } else if ($(this).prop('id') === 'btn-8') {
+      board.square8 = $(this).text()
+    } else if ($(this).prop('id') === 'btn-9') {
+      board.square9 = $(this).text()
+    }
 }
 
 const gameHandlers = () => {
