67fb83fc48e45a8a125264a10dbf5f6d9a4e0f77
HTarzwell <htarzwell@gmail.com>
2018-10-07 15:24:27 -0400

    Began game update process JSON and new curl

--- assets/scripts/games/game-api.js
+++ assets/scripts/games/game-api.js
@@ -4,10 +4,21 @@ const config = require('../config')
 const store = require('../store')
 
 const createGame = function (data) {
-  console.log('api firing, data in create game is ', data)
   return $.ajax({
     url: config.apiOrigin + '/games',
     method: 'POST',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const changePassword = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/games/' + store.game.id,
+    method: 'PATCH',
     headers: {
       contentType: 'application/json',
       Authorization: 'Token token=' + store.user.token
---
+++ curl-scripts/games/retrieve-game.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl "https://aqueous-atoll-85096.herokuapp.com/games/${ID}" \
+  --include \
+  --request GET \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+
+echo
